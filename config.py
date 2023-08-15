b4156d4ed689d178197a06f31d3fbdbb71bfccc3
mohamedayman829 <142358999+mohamedayman829@users.noreply.github.com>
2023-08-16 02:08:32 +0300

    Update config.py

--- config.py
+++ config.py
@@ -13,15 +13,15 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
 OWNER_ID = int(getenv("OWNER_ID"))
 
-PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/8fa109bec9bb4dcf64e74.mp4")
-START_IMG = getenv("START_IMG", "https://telegra.ph/file/2514530559cc173845e3f.jpg")
+PING_IMG = getenv("PING_IMG", "https://telegra.ph/Shaher-08-15")
+START_IMG = getenv("START_IMG", "https://graph.org/Photo-07-29-20")
 
 SESSION = getenv("SESSION", None)
 
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/SORS0C")
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/UIU_II")
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6300938349").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5134002906").split()))
 
 
 FAILED = "https://te.legra.ph/file/e906c2def5afe8a9b9120.jpg"
